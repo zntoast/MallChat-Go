768ede9c6ee30fe1ebb643fe161c2f2d02b31882
ZaiNeng <2878428463@qq.com>
2024-11-19 18:17:18 +0800

    first commit

---
+++ app/user/internal/handler/login/loginhandler.go
@@ -0,0 +1,30 @@
+package login
+
+import (
+	"net/http"
+
+	"mallchat-go/app/user/internal/logic/login"
+	"mallchat-go/app/user/internal/svc"
+	"mallchat-go/app/user/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+// login
+func LoginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserLoginReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := login.NewLoginLogic(r.Context(), svcCtx)
+		err := l.Login(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.Ok(w)
+		}
+	}
+}
---
+++ app/user/internal/logic/login/loginlogic.go
@@ -0,0 +1,31 @@
+package login
+
+import (
+	"context"
+
+	"mallchat-go/app/user/internal/svc"
+	"mallchat-go/app/user/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type LoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// login
+func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
+	return &LoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *LoginLogic) Login(req *types.UserLoginReq) error {
+	// todo: add your logic here and delete this line
+
+	return nil
+}
